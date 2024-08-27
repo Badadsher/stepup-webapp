c9fb8a26a9a1208aa94c12264a209498cd288172
Joe Biden <dominserv123@mail.ru>
2024-08-27 20:56:31 +0300

    centerSection

--- src/components/CenterSection/CenterSection.jsx
+++ src/components/CenterSection/CenterSection.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "../CenterSection/centersection.css";
 import loop from "../images/loop.png";
+import faq from "../images/faq.png";
+import brends from "../images/brends.png";
+import about from "../images/about.png";
+import logostep from "../images/logostepsv.svg";
+
 function CenterSection() {
   return (
     <div className="centersection">
@@ -9,6 +14,36 @@ function CenterSection() {
         <button className="searchBt">
           <img src={loop}></img>
         </button>
+      </div>
+
+      <div className="centersection-down">
+        <div className="centersection-down_left">
+          <div className="faqBt">
+            <button>
+              <img src={faq}></img>
+            </button>
+            <a>
+              ЧАСТЫЕ <br /> ВОПРОСЫ
+            </a>
+          </div>
+          <div className="aboutBt">
+            <button>
+              <img src={about}></img>
+            </button>
+            <a>О НАС</a>
+          </div>
+        </div>
+
+        <div className="centersection-down_right">
+          <button>
+            <img src={brends}></img>
+            <a>ВСЕ БРЕНДЫ</a>
+          </button>
+          <button>
+            <img src={logostep}></img>
+            <a>НАШ ТЕЛЕГРАМ</a>
+          </button>
+        </div>
       </div>
     </div>
   );
