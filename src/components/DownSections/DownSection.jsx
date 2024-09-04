ef486374f56e99594cfd5eb1c450645faee2f18b
Joe Biden <dominserv123@mail.ru>
2024-09-04 16:05:45 +0300

    newCardsDesign

--- src/components/DownSections/DownSection.jsx
+++ src/components/DownSections/DownSection.jsx
@@ -1,23 +1,22 @@
 import React from "react";
-import NikeData from "../Sneakers/nike.json";
+import MainPage from "../Sneakers/mainpage.json";
 import "../DownSections/downSection.css";
 function DownSection() {
   return (
     <div className="down-section">
       <a className="down-section-logo">ПОПУЛЯРНОЕ</a>
       <div className="menu-container">
-        {NikeData.map((item, index) => (
+        {MainPage.map((item, index) => (
           <div key={index} className="sneaker-card">
             <div>
               {" "}
               <img src={item.image}></img>
+              <a>{item.name}</a>
+              <a>{item.price}</a>
+              <button onClick={() => handleButtonClick(index, item)}>
+                ЗАКАЗАТЬ
+              </button>
             </div>
-
-            <a>{item.name}</a>
-            <a>{item.price}</a>
-            <button onClick={() => handleButtonClick(index, item)}>
-              ЗАКАЗАТЬ
-            </button>
           </div>
         ))}
       </div>
