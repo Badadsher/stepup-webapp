a6ae509fc95fbc22e159eb0c00384d0a68b2fc7d
Joe Biden <dominserv123@mail.ru>
2024-09-20 16:26:57 +0300

    fix

--- src/components/DownSections/DownSection.jsx
+++ src/components/DownSections/DownSection.jsx
@@ -34,7 +34,7 @@ function DownSection() {
     let message = "Ваш заказ принят!";
     window.Telegram.WebApp.showAlert(message);
     // Отправка данных на сервер
-    fetch("http://localhost:8080/stepup/checker.php", {
+    fetch("http://45.140.179.231/stepchater/checker.php", {
       mode: "no-cors",
       method: "POST",
       headers: {
