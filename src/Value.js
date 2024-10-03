55482afe8c2ae1feaa6918d4d5d557a45aa16429
mxnnypsswrds <145398271+mxnnypsswrds@users.noreply.github.com>
2024-10-03 10:37:51 +0300

    21

--- src/Value.js
+++ src/Value.js
@@ -1,6 +1,6 @@
-let dramForYuan = 56;
-let dramForRub = 4.2;
+let dramForYuan = 56.67;
+let dramForRub = 4.05;
 let yuanForEur = 0.13;
-let eurForRub = 100;
+let eurForRub = 104.45;
 
 export { dramForYuan, dramForRub, yuanForEur, eurForRub };
